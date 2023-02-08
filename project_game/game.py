dc31406081f32e612c498ed0e436c4215d4da587
Margarita <podezzhikh95@bk.ru>
2023-02-09 00:02:50 +0300

    Creating a game board

--- project_game/game.py
+++ project_game/game.py
@@ -7,4 +7,21 @@ def greet():
     print("    y - номер столбца    ")
     print("-------------------------")
 
+
 greet()
+
+board = [[" ", " ", " "] for i in range(3)]
+
+
+def draw_board():
+    print()
+    print("   | 0 | 1 | 2 |   ")
+    print("----------------")
+    for i, row in enumerate(board):
+        row_str = f" {i} | {' | '.join(row)} | "
+        print(row_str)
+        print("----------------")
+    print()
+
+
+draw_board()
