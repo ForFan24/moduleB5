d5f0d783aa673029d700ea878c55e03afe3395a7
Margarita <podezzhikh95@bk.ru>
2023-02-09 00:33:08 +0300

    Adding user coordinate input

--- project_game/game.py
+++ project_game/game.py
@@ -25,3 +25,25 @@ def draw_board():
 
 
 draw_board()
+
+def ask():
+    while True:
+        cords = input("Введите координаты клетки: ").split()
+        if len(cords) != 2:
+            print("Введите две координаты")
+            continue
+        x, y = cords
+        if not (x.isdigit()) or not (y.isdigit()):
+            print("Введите числа!")
+            continue
+        x, y = int(x), int(y)
+        if 0 > x or x > 2 or 0 > y or y > 2:
+            print("Координаты вне диапазона!")
+            continue
+        if board[x][y] != " ":
+            print("Клетка занята")
+            continue
+        return x, y
+
+
+ask()
